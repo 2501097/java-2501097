f3b7ab8986517bb3c183c9377cd41e597de968ad
2501097 <sion3670@kbu.ac.kr>
2025-10-15 13:12:39 +0900

    5주차 report

---
+++ week04/sample0/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        System.out.printf("a = %d, b = %d", ++a, b++);
+        System.out.printf("\n a = %d, b = %d", a, b);
+    }
+}
