9697279289f622c81cb125dbaa4c3b05a7982e01
2501097 <sion3670@kbu.ac.kr>
2025-11-12 09:52:40 +0900

    9주차

---
+++ week09/sample 9-1/src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    /*    System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you.");
+        System.out.println("I love you."); */
+
+        for (int i =1; 1 <= 10; i++) {
+            System.out.printf("%2dI love you.\n", i);
+        }
+        int i =1;
+        while (i <= 10) { // i = 제어변수
+            System.out.printf("%2dI love you.\n", i++);
+        }
+
+
+    }
+}
